43e3286ba59b9b9d5c553e68dfe3c0cb456c401c
ligi <ligi@ligi.de>
2013-04-12 21:16:08 +0200

    close stuff

--- src/org/ligi/fast/util/FileHelper.java
+++ src/org/ligi/fast/util/FileHelper.java
@@ -6,32 +6,35 @@ import java.io.IOException;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 import java.nio.charset.Charset;
+
 /**
  * Helper to read the content of a File
- * 
+ *
  * @author Marcus -ligi- Büschleb
- * 
- * License GPLv3
+ *         <p/>
+ *         License GPLv3
  */
 public class FileHelper {
 
-	/**
-	 * reads a file to a string
-	 * 
-	 * @param file
-	 * @return the content of the file
-	 * @throws IOException
-	 */
-	public static String file2String(File file) throws IOException {
-		FileInputStream stream = new FileInputStream(file);
-		try {
-			FileChannel fc = stream.getChannel();
-			MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0,
-					fc.size());
-			return Charset.defaultCharset().decode(bb).toString();
-		} finally {
-			stream.close();
-		}
+    /**
+     * reads a file to a string
+     *
+     * @param file
+     * @return the content of the file
+     * @throws IOException
+     */
+    public static String file2String(File file) throws IOException {
+        FileInputStream stream = new FileInputStream(file);
+        try {
+            FileChannel fc = stream.getChannel();
+            MappedByteBuffer bb = fc.map(FileChannel.MapMode.READ_ONLY, 0,
+                    fc.size());
+            fc.close();
+            stream.close();
+            return Charset.defaultCharset().decode(bb).toString();
+        } finally {
+            stream.close();
+        }
 
-	}
+    }
 }
