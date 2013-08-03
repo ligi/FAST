8ab3dd89402449536d806b27d5153fbf07173eac
ligi <ligi@ligi.de>
2013-08-03 17:31:15 +0200

    initial fdroid as flavor test

---
+++ src/forFDroid/java/fast/TargetStore.java
@@ -0,0 +1,11 @@
+package org.ligi.fast;
+
+/**
+ * Created by ligi on 6/1/13.
+ */
+public class TargetStore {
+    public final static String STORE_URL = "http://play.google.com/store/apps/details?id=";
+    public final static String STORE_NAME = "Google Play";
+    public final static String STORE_PNAME = "com.android.vending";
+
+}
