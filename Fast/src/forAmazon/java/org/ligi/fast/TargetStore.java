2847b258b1f05cbf254eb4670039cdbb71b375b9
ligi <ligi@ligi.de>
2013-09-04 11:05:58 +0200

    rm ego

--- Fast/src/forAmazon/java/org/ligi/fast/TargetStore.java
+++ Fast/src/forAmazon/java/org/ligi/fast/TargetStore.java
@@ -1,8 +1,5 @@
 package org.ligi.fast;
 
-/**
- * Created by ligi on 6/1/13.
- */
 public class TargetStore {
     public final static String STORE_URL = "http://www.amazon.com/gp/mas/dl/android?p=";
     public final static String STORE_NAME = "Amazon Store";
