48eda9091ca925473789bb002df3f115004273c0
ligi <ligi@ligi.de>
2013-09-24 22:48:42 +0200

    initial hook into the broadcast

---
+++ app/src/main/java/org/ligi/fast/AppInstallOrRemoveReceiver.java
@@ -0,0 +1,14 @@
+package org.ligi.fast;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+
+import org.ligi.tracedroid.logging.Log;
+
+public class AppInstallOrRemoveReceiver extends BroadcastReceiver{
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.w("Broadcast rcv" + intent.getAction() + " " + intent.getData() );
+    }
+}
