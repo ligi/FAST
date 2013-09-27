6c4b753da6aa1b5b4e1685420751da50724734a7
ligi <ligi@ligi.de>
2013-09-27 12:56:35 +0200

    use SEPARATOR const

--- app/src/main/java/org/ligi/fast/util/PackageListStore.java
+++ app/src/main/java/org/ligi/fast/util/PackageListStore.java
@@ -58,7 +58,7 @@ public class PackageListStore {
 
         for (AppInfo appInfo : appInfoList) {
             res.append(appInfo.toCacheString());
-            res.append("\n");
+            res.append(SEPARATOR);
         }
 
         try {
