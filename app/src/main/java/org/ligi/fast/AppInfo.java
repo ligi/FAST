372600a414cba24b02191a1c9dd080d94a29bd0d
talexop <talexop@gmail.com>
2013-09-27 14:13:25 +0300

    I forgot to add drawabletobitmap function!!
    I forgot to add drawabletobitmap function!!

--- app/src/main/java/org/ligi/fast/AppInfo.java
+++ app/src/main/java/org/ligi/fast/AppInfo.java
@@ -119,6 +119,19 @@ public class AppInfo {
         }
            
     }
+    
+    public static Bitmap drawableToBitmap (Drawable drawable) {
+        if (drawable instanceof BitmapDrawable) {
+            return ((BitmapDrawable)drawable).getBitmap();
+        }
+
+        Bitmap bitmap = Bitmap.createBitmap(drawable.getIntrinsicWidth(), drawable.getIntrinsicHeight(), Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap); 
+        drawable.setBounds(0, 0, canvas.getWidth(), canvas.getHeight());
+        drawable.draw(canvas);
+
+        return bitmap;
+    }
 
     private void cacheIcon(ResolveInfo ri) {
 
