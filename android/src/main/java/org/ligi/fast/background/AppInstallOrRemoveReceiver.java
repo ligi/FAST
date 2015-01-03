0020422c63854c35761b8c165d70dc5260e016b1
ligi <ligi@ligi.de>
2015-01-03 18:21:25 +0100

    fix a problem when app was installed/removed when fast is in background

--- android/src/main/java/org/ligi/fast/background/AppInstallOrRemoveReceiver.java
+++ android/src/main/java/org/ligi/fast/background/AppInstallOrRemoveReceiver.java
@@ -4,11 +4,24 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 
+import org.ligi.fast.App;
+import org.ligi.fast.model.AppInfoList;
 import org.ligi.fast.util.AppInfoListStore;
 
 public class AppInstallOrRemoveReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        new BackgroundGatherAsyncTask(context, new AppInfoListStore(context).load()).execute();
+        final AppInfoListStore appInfoListStore = new AppInfoListStore(context);
+
+        if (App.packageChangedListener==null) {
+            App.packageChangedListener= new App.PackageChangedListener() {
+                @Override
+                public void onPackageChange(AppInfoList appInfoList) {
+                    appInfoListStore.save(appInfoList);
+                }
+            };
+        }
+
+        new BackgroundGatherAsyncTask(context, appInfoListStore.load()).execute();
     }
 }
