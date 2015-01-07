a53aed5e6ea910f11dcd61ef085c6773a772587a
cketti <cketti@gmail.com>
2015-01-07 06:16:05 +0100

    Use proper layout for help dialog
    This way we can easily add padding in DIPs.

--- android/src/withExtras/java/org/ligi/fast/ui/HelpDialog.java
+++ android/src/withExtras/java/org/ligi/fast/ui/HelpDialog.java
@@ -1,11 +1,14 @@
 package org.ligi.fast.ui;
 
+import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.text.Html;
 import android.text.method.LinkMovementMethod;
 import android.text.util.Linkify;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.widget.TextView;
 
 import org.ligi.fast.R;
@@ -13,14 +16,13 @@ import org.ligi.fast.R;
 class HelpDialog {
 
     public static void show(Context ctx) {
-
-        TextView tv = new TextView(ctx);
-        tv.setPadding(10, 10, 10, 10);
+        @SuppressLint("InflateParams") View view = LayoutInflater.from(ctx).inflate(R.layout.help_dialog, null);
+        TextView tv = (TextView) view.findViewById(R.id.help_text);
         tv.setText(Html.fromHtml(ctx.getString(R.string.help_content)));
-
         Linkify.addLinks(tv, Linkify.ALL);
         tv.setMovementMethod(LinkMovementMethod.getInstance());
-        new AlertDialog.Builder(ctx).setTitle(R.string.help_label).setView(tv)
+
+        new AlertDialog.Builder(ctx).setTitle(R.string.help_label).setView(view)
                 .setPositiveButton(android.R.string.ok, new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
