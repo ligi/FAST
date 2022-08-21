678ae003a2a7640d0670c9770a77794238b74a35
Sebastian <SebiderSushi@t-online.de>
2022-08-21 05:06:55 +0200

    feat: use https in sharing links

--- android/src/forAmazon/java/org/ligi/fast/TargetStore.java
+++ android/src/forAmazon/java/org/ligi/fast/TargetStore.java
@@ -1,7 +1,7 @@
 package org.ligi.fast;
 
 public class TargetStore {
-    public final static String STORE_URL = "http://www.amazon.com/gp/mas/dl/android?p=";
+    public final static String STORE_URL = "https://www.amazon.com/gp/mas/dl/android?p=";
     public final static String STORE_NAME = "Amazon Store";
     public final static String STORE_PNAME = "com.amazon.venezia";
 }
--- android/src/forFDroid/java/fast/TargetStore.java
+++ android/src/forFDroid/java/fast/TargetStore.java
@@ -1,7 +1,7 @@
 package org.ligi.fast;
 
 public class TargetStore {
-    public final static String STORE_URL = "http://play.google.com/store/apps/details?id=";
+    public final static String STORE_URL = "https://play.google.com/store/apps/details?id=";
     public final static String STORE_NAME = "Google Play";
     public final static String STORE_PNAME = "com.android.vending";
 }
