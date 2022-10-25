5bdef49429e965d525dcd16f7d697725cf2108e4
Sebastian <SebiderSushi@t-online.de>
2022-10-25 08:28:44 +0200

    refactor: remove unused ViewHelper.hideKeyBoard and .startIntentOnClick

--- android/src/main/java/org/ligi/axt/helpers/ViewHelper.java
+++ android/src/main/java/org/ligi/axt/helpers/ViewHelper.java
@@ -2,7 +2,6 @@ package org.ligi.axt.helpers;
 
 
 import android.content.Context;
-import android.content.Intent;
 import android.util.Log;
 import android.view.View;
 import android.view.inputmethod.InputMethodManager;
@@ -15,15 +14,6 @@ public class ViewHelper {
         this.view = view;
     }
 
-    public void hideKeyBoard() {
-        InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
-        if (imm != null) {
-            imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
-        } else {
-            Log.w("AndroidHelper", "could not hide Keyboard as INPUT_METHOD_SERVICE is not available");
-        }
-    }
-
     public void showKeyboard() {
         InputMethodManager imm = (InputMethodManager) view.getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
         if (imm != null) {
@@ -32,15 +22,4 @@ public class ViewHelper {
             Log.w("AndroidHelper", "could not show Keyboard as INPUT_METHOD_SERVICE is not available");
         }
     }
-
-    public void startIntentOnClick(final Intent intent) {
-        view.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v.getContext().startActivity(intent);
-            }
-        });
-    }
-
-
 }
